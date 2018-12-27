a548462df6e58177e1edfeac252c96a8feb9d37e
Prabhu <prabhu.raja@rci.rogers.com>
2018-12-26 20:44:13 -0500

    Create Server

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Primary File for the API
+ */
+
+// Dependencies
+const http = require('http');
+
+// The server should respond  to all requests with a string
+const server = http.createServer((req, res) => {
+    res.end(`Hello World 👍`);
+});
+
+// Start a server, and have it listen on port 3000
+server.listen(3000, () => console.log('The Server is listening on Port 3000🎧'));
