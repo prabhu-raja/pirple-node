093b8d93302d2f70d9e999f5d8d72a74d6940f80
Prabhu <prabhu.raja@rci.rogers.com>
2019-02-19 16:10:36 -0500

    Parsed path

--- index.js
+++ index.js
@@ -4,10 +4,27 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
+
 
 // The server should respond  to all requests with a string
 const server = http.createServer((req, res) => {
+
+    // Get the URL and parse it
+    const parsedUrl = url.parse(req.url, true);
+    console.log(`Parsed Url - ${JSON.stringify(parsedUrl)}`);
+
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response
     res.end(`Hello World from server`);
+
+
+    // Log the request path
+    console.log(`Req received on path🐛 - ${trimmedPath}`);
 });
 
 // Start a server, and have it listen on port 3000
